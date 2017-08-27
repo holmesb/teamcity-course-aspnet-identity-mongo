5d75400dcf20b3a179c80b06fa763e5d8f4e31bd
holmesb <me@brendanh.com>
2017-08-27 13:02:26 +0100

    fixing hopefully

--- src/Tests/IdentityRoleTests.cs
+++ src/Tests/IdentityRoleTests.cs
@@ -35,9 +35,8 @@
 
 			var role = new IdentityRole(name);
 
-			Expect(role.Name, Is.EqualTo("notadmin"));
+			Expect(role.Name, Is.EqualTo(name));
 		}
-#comment
 
 		[Test]
 		public void Create_WithRoleName_SetsId()
